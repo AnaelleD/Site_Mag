68b26a9dd199d6a108b6356af342d39a16f10429
AnaelleD <anaelle.dambreville@gmail.com>
2018-06-10 22:36:01 +0200

    insertion image BD

--- bdd/creationBdd.sql
+++ bdd/creationBdd.sql
@@ -7,3 +7,11 @@ create table Image (
     categorie varchar(20),
     image OID
 );
+
+insert into Image(nom,legende,categorie,image)
+  values('bad girl', 'Affiche bad Alès', 'publicite', lo_import('C:\wamp64\www\Site_Mag\image\bad_girl.jpg'));
+
+insert into Image(nom,legende,categorie,image)
+    values('cimetiere_sans_pesticide', 'Affiche cimetière sans pesticide',
+      'publicite', lo_import('C:\wamp64\www\Site_Mag\image\cimetiere_sans_pesticide.jpg'));
+      

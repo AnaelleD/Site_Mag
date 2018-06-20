5bb4b7d5c9ea588b3fa7f50ccb090e0452664699
dmasclef <diane.masclef@gmail.com>
2018-06-20 16:20:06 +0200

    pusher pour pusher

--- bdd/creationBdd.sql
+++ bdd/creationBdd.sql
@@ -29,6 +29,6 @@ insert into Image(nom,legende,categorie,image)
 insert into Image(nom,legende,categorie,photo)
   values('bad girl', 'Affiche bad Alès', 'publicite', '../image/bad_girl.jpg');
 
-insert into Image2(nom,legende,categorie,photo)
+insert into Image(nom,legende,categorie,photo)
     values('cimetiere_sans_pesticide', 'Affiche cimetière sans pesticide',
       'creativite', '../image/cimetiere_sans_pesticide.jpg');
--- script/portfolio.php
+++ script/portfolio.php
@@ -4,7 +4,7 @@ $bdd = connexionbd();
 ?>
 
 <?php
-echo '<table width = "100%" border = "1">';
+/*echo '<table width = "100%" border = "1">';
 	echo '<tr>';
     echo'<th>nom</th>';
 		echo'<th>legende</th>';
@@ -25,5 +25,16 @@ foreach($resultat as $ligne) {
 	}
 
 echo ('</table>');
+*/
 
+$requete = "select nom,legende, categorie, photo from image where categorie = 'creativite'";
+$resultat = requete($bdd, $requete);
+
+foreach($resultat as $ligne) {
+	echo '<article class = "portfolioCreativite" style = "display: inline-block; z-index: 10; opacity: 1">';
+		echo '<span class = image>';
+			  echo ('<img src ="'.$ligne['photo'].'"width=150" "height=150"/>&nbsp');
+		echo '</span>';
+	echo '</article>';
+}
 ?>

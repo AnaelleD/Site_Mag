c23e677c8132906a1c563d878ade386213715c75
AnaelleD <anaelle.dambreville@gmail.com>
2018-07-25 16:16:43 +0200

    Ajout watermark

---
+++ watermark.php
@@ -0,0 +1,20 @@
+<?php
+	// Charge le cachet et la photo afin d'y appliquer le tatouage numérique
+	$watermark = imagecreatefrompng('image/watermark.png');
+	$im = imagecreatefromjpeg($_GET['image']);
+
+	// Définit les marges pour le cachet et récupère la hauteur et la largeur de celui-ci
+	$marge_right = 10;
+	$marge_bottom = 10;
+	$sx = imagesx($watermark);
+	$sy = imagesy($watermark);
+
+	// Copie le cachet sur la photo en utilisant les marges et la largeur de la
+	// photo originale  afin de calculer la position du cachet
+	imagecopy($im, $watermark, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($watermark), imagesy($watermark));
+
+	// Affichage et libération de la mémoire
+	header('Content-type: image/png');
+	imagepng($im);
+	imagedestroy($im);
+?>

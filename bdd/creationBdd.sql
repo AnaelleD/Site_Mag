b21b646a701e0c4bea16d09c8e3c1171e55587d3
dmasclef <diane.masclef@gmail.com>
2018-11-23 11:38:27 +0100

    mise à jour bdd + images

--- bdd/creationBdd.sql
+++ bdd/creationBdd.sql
@@ -8,13 +8,63 @@ create table image (
      photo VARCHAR(200)
 );
 
+<--!insert into Image(nom,legende,categorie,photo)-->
+  <!--values('bad girl', 'Affiche bad Alès', 'publicite', 'image/bad_girl.jpg');-->
+
+# communication
 insert into Image(nom,legende,categorie,photo)
-  values('bad girl', 'Affiche bad Alès', 'publicite', 'image/bad_girl.jpg');
+    values('captage apercu net1', 'captage', 'communication', 'image/communication/captage_APERCU_net_1.jpg');
 
 insert into Image(nom,legende,categorie,photo)
+    values('carte couleur', 'carte couleur', 'communication', 'image/communication/carte_couleur_reduit.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('carte mada 1', 'carte mada', 'communication', 'image/communication/cartemada1.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('carte mada 2', 'carte mada', 'communication', 'image/communication/cartemada2.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('carte mada 3', 'carte mada', 'communication', 'image/communication/cartemada3.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('carte mada 4', 'carte mada', 'communication', 'image/communication/cartemada4.jpg');
+
+# passion
+insert into Image(nom,legende,categorie,photo)
+    values('photoA1', 'A1', 'passion', 'image/passion/A1.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('photoA2', 'A2', 'passion', 'image/passion/A2.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('photoA3', 'A3', 'passion', 'image/passion/A3.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('photoA4', 'A4', 'passion', 'image/passion/A4.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('photoA5', 'A5', 'passion', 'image/passion/A5.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('photoA6', 'A6', 'passion', 'image/passion/A6.jpg');
+
+# personnalisation
+insert into Image(nom,legende,categorie,photo)
+    values('bad girl', 'Affiche bad Alès', 'personnalisation', 'image/personnalisation/bad_girl.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('tournoi ours', 'Affiche tournoi ours', 'personnalisation', 'image/personnalisation/tournoi_ours.jpg');
+
+insert into Image(nom,legende,categorie,photo)
+    values('tournoi SGSC', 'Affiche tournoi SGSC', 'personnalisation', 'image/personnalisation/tournoi_SGSC.jpg');
+
+
+# portfolio
+insert into Image(nom,legende,categorie,photo)
     values('cimetiere_sans_pesticide', 'Affiche cimetière sans pesticide',
-      'creativite', 'image/cimetiere_sans_pesticide.jpg');
+      'portfolio', 'image/cimetiere_sans_pesticide.jpg');
 
 insert into Image(nom,legende,categorie,photo)
     values('chien', 'Un petit chien',
-      'creativite', 'image/chien.jpg');
+      'portfolio', 'image/chien.jpg');
